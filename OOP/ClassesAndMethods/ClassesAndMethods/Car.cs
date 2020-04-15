f9a54a60d067b0426b0892e2614049423ed167a0
ancor1369 <ancor1369@gmail.com>
2020-04-14 21:13:43 -0400

    Start explaining programming concepts for Katherine, We created a class and explained some logics

---
+++ OOP/ClassesAndMethods/ClassesAndMethods/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    //Accesibility modifier, keywork class , Class name
+    public class Car
+    {
+        //Class members
+
+        //Properties
+        public string Color { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+
+        //Field
+        public bool isEngineOn;
+
+        public string turnEngineOn()
+        {
+            if (isEngineOn == true)
+            {
+                return "Engine is already on";
+            }
+            //if(isEngineOn == false)Creates error
+            else //this is the  when isEngineOn is false
+            {
+                isEngineOn = true;
+                return "Engine is on";
+            }
+        }
+
+        public string turnEngineOff()
+        {
+            if (isEngineOn == true)
+            {
+                isEngineOn = false;
+                return "Engine is off";
+            }
+            else
+            {
+                return "Engine is already off";
+            }
+        }
+
+    }
+}
---
+++ OOP/ClassesAndMethods/ClassesAndMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Height = 45;            
+
+            string result = myCar.turnEngineOff();
+            Console.WriteLine(result);
+            result = myCar.turnEngineOn();
+            Console.WriteLine(result);
+            result = myCar.turnEngineOn();
+            Console.WriteLine(result);
+            result = myCar.turnEngineOff();
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+        }
+    }     
+}
